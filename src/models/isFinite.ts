7bcf66dbd4e7e055767da859a0e280c0581ad952
tm.infineo <thibault.meyer@infineo.fr>
2024-06-27 13:01:37 +0200

    [Remove] Old commented IsFinite type

--- src/models/isFinite.ts
+++ src/models/isFinite.ts
@@ -2,22 +2,6 @@ import { IsDefinedNumber } from "./isDefinedNumber"
 import { Length } from "./length"
 import { List } from "./types"
 
-// ! Keep it in case the new version doesn't work as expected
-// export type IsFinite<A extends List, Finite = true, Infinite = false> = {
-//     empty: Finite,
-//     notEmpty: ((...args: A) => any) extends ((_firstArgs: infer First, ...args: infer Rest) => any)
-//     ? IsFinite<Rest, Finite, Infinite>
-//     : never
-//     infinite: Infinite
-// }[
-//     A extends [] ? 'empty'
-//     : A extends (infer Element)[] ?
-//     Element[] extends A ?
-//     'infinite'
-//     : 'notEmpty'
-//     : never
-// ]
-
 // Check if the given array has a finite length
 export type IsFinite<
     A extends List,
