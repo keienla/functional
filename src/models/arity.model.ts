16e4e4025bef2ce05031702498f9c2053f34d816
tm.infineo <thibault.meyer@infineo.fr>
2024-06-27 12:50:44 +0200

    [Update] Simplify the type of Arity

--- src/models/arity.model.ts
+++ src/models/arity.model.ts
@@ -1,6 +1,3 @@
-import { Before, Cast, Concat, Fn, Head, IsDefinedNumber, IsNever, Iterator, Last, Length, Pop, Tail, Tuple } from "./utils";
+import { Before, Cast, Fn, IsDefinedNumber } from "./utils";
 
-export type Arity<F extends Fn, Size extends number> =
-    IsDefinedNumber<Size> extends true
-    ? (...args: Cast<Before<Size, Parameters<F>>, any[]>) => ReturnType<F>
-    : () => ReturnType<F>
+export type Arity<F extends Fn, Size extends number> = (...args: Cast<Before<Size, Parameters<F>>, any[]>) => ReturnType<F>
