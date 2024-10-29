79855546194794ec80004597030a28726c52149a
tm.infineo <thibault.meyer@infineo.fr>
2024-10-29 18:00:02 +0100

    [WIP](tupleFromUnion) More accurate name in type

--- src/models/tupleFromUnion.ts
+++ src/models/tupleFromUnion.ts
@@ -6,12 +6,12 @@ import { IsNever } from "./isNever"
 
 // While RemainingUnion is not empty, run and add the last property at the beginning of CurrentTuple
 export type TupleFromUnion<RemainingUnion, CurrentTuple extends List = []> = {
-    0: TupleFromUnion<PopUnion<RemainingUnion>, PrependItem<LastUnion<RemainingUnion>, CurrentTuple>>
-    1: CurrentTuple
+    Continue: TupleFromUnion<PopUnion<RemainingUnion>, PrependItem<LastUnion<RemainingUnion>, CurrentTuple>>
+    Finish: CurrentTuple
 }[
-    IsNever<RemainingUnion, 1, 0>
+    IsNever<RemainingUnion, 'Finish', 'Continue'>
 ]
 
 type testTupleFromUnion1 = TupleFromUnion<'test1' | 'test2' | 'test3'>  // ['test1', 'test2', 'test3']
 type testTupleFromUnion2 = TupleFromUnion<string | number>              // [string, number]
-type testTupleFromUnion3 = TupleFromUnion<number>                       // [string, number]
+type testTupleFromUnion3 = TupleFromUnion<number>                       // [number]
